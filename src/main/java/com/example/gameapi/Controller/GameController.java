d98fa1b618b188f015c2107cd2b7104d7d17feac
Tobi8570 <73465176+Tobi8570@users.noreply.github.com>
2024-02-20 09:01:22 +0100

    Put method works, so we can create, read, update & delete

--- src/main/java/com/example/gameapi/Controller/GameController.java
+++ src/main/java/com/example/gameapi/Controller/GameController.java
@@ -5,23 +5,26 @@ import com.example.gameapi.model.Game;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
+import static com.example.gameapi.model.Data.games;
 
 @RestController
 @RequestMapping("api/games")
 public class GameController {
     @GetMapping
-    public List<Game> getAllGames(){
-        return Data.games;
+    public List<Game> getAllGames() {
+        return games;
     }
 
     @PostMapping
-    void createGame(@RequestBody Game game){
-        Data.games.add(game);
+    void createGame(@RequestBody Game game) {
+        games.add(game);
     }
 
     @GetMapping("/{id}")
-    Game getGameById(@PathVariable int id){
-        Game gameFound = Data.games.stream()
+    Game getGameById(@PathVariable int id) {
+        Game gameFound = games.stream()
                 .filter(game -> id == (game.getId()))
                 .findFirst()
                 .orElse(null);
@@ -30,15 +33,24 @@ public class GameController {
 
 
     @DeleteMapping("/{id}")
-    Boolean deleteGameById(@PathVariable int id){
-        Game gameFound = Data.games.stream()
+    Boolean deleteGameById(@PathVariable int id) {
+        Game gameFound = games.stream()
                 .filter(game -> id == (game.getId()))
                 .findFirst()
                 .orElse(null);
-        if(gameFound != null){
-            Data.games.remove(gameFound);
+        if (gameFound != null) {
+            games.remove(gameFound);
             return true;
         }
         return false;
+    }
+
+    @PutMapping()
+    public Boolean updateGameById(@RequestBody Game updatedGame) {
+       for(Game gg : Data.games){
+           Data.games.set(Data.games.indexOf(gg), updatedGame);
+           return true;
+       }
+       return false;
     }
 }
